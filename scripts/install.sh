a468b3d132848f0912313145b644bfc0183e9eda
nathanasiou <nathanasiou@ladmins-MacBook-Air-3.local>
2022-09-11 23:25:37 +0200

    ignore environments, test reqs

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,7 @@ python3.9 -m venv teach-env
 echo "Activating virtual environment"
 
 source $PWD/teach-env/bin/activate
+$PWD/teach-env/bin/pip install --upgrade pip setuptools
 
 $PWD/teach-env/bin/pip install torch==1.11.0 torchmetrics==0.7.2 torchvision==0.12.0 numpy==1.22.3
 $PWD/teach-env/bin/pip install -r requirements.txt
